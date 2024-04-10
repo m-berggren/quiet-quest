71dc24d6d931a068d1642b9434fbd2fdb263b0aa
Julia McCall <mccall@student.chalmers.se>
2024-04-10 16:59:56 +0200

    Add escape hatch to Create Quest screen

--- app/src/main/java/quietquest/app/CreateQuestController.java
+++ app/src/main/java/quietquest/app/CreateQuestController.java
@@ -2,11 +2,17 @@ package quietquest.app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -19,10 +25,13 @@ public class CreateQuestController {
     private TextArea descriptionField;
     @FXML
     private TextField taskDescriptionField;
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
     private HashMap<String, Quest> quests;
 
 
-    // create a quest:
+    // create a quest by clicking "Save Quest" button:
     public void createQuest() {
         String title = titleField.getText();
         String description = descriptionField.getText();
@@ -33,5 +42,16 @@ public class CreateQuestController {
         quests.put(title, quest);
     }
 
+    // cancel quest creation by clicking "Cancel" button:
+    @FXML
+    protected void cancelQuestCreation (ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(QuietQuestMain.class.getResource("/quietquest/app/hello-view.fxml"));
+        root = loader.load();
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }

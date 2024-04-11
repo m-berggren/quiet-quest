32116737db6c3b13d9d81e74d791ec249296d42c
Tanya Nordh <tanyan@net.chalmers.se>
2024-04-11 14:18:52 +0200

    Add quest list view

--- app/src/main/java/quietquest/app/QuietQuestMain.java
+++ app/src/main/java/quietquest/app/QuietQuestMain.java
@@ -11,9 +11,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class QuietQuestMain extends Application {
+    public static QuestManager questManager;
 
     @Override
     public void start(Stage stage) throws IOException {
+        questManager = new QuestManager();
         FXMLLoader fxmlLoader = new FXMLLoader(QuietQuestMain.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1510, 760);
         stage.setTitle("Hello!");

e026bdb3413cfe3338bbdf2e4c28b4f77418fec3
marcusberggren <berggren.me@outlook.com>
2024-04-12 10:04:51 +0200

    Module-info and Gradle build updated

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module quietquest.app {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.hivemq.client.mqtt;
 
     opens quietquest.app to javafx.fxml;
     exports quietquest.app;

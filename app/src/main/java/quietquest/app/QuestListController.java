e10ef697249546d5287bd9beba9df89565de6104
Tanya Nordh <tanyan@net.chalmers.se>
2024-04-14 13:25:54 +0200

    move mqtt labels and buttons from start view to quest list view

--- app/src/main/java/quietquest/app/QuestListController.java
+++ app/src/main/java/quietquest/app/QuestListController.java
@@ -1,4 +1,5 @@
 package quietquest.app;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -8,38 +9,42 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.*;
 
-public class QuestListController implements Initializable {
+public class QuestListController implements Initializable, UIUpdater {
     @FXML
     private Button deleteButton;
     @FXML
     private ListView<String> questListView;
-
-    private QuestManager questManager;
-
-    private HashMap<String, Quest> quests;
+    @FXML
+    private ToggleButton subscribeButton;
+    @FXML
+    private Label mqttMessage;
+    @FXML
+    private Label mqttDistanceMessage;
 
     private FXMLLoader loader;
     private Parent root;
     private Stage stage;
     private Scene scene;
+    private QuestManager questManager;
+    private HashMap<String, Quest> quests;
+    public MQTTHandler mqttClient;
+
 
     public void initialize(URL arg0, ResourceBundle arg1) {
         questManager = QuietQuestMain.questManager;
         quests = questManager.getQuests();
+        mqttClient = new MQTTHandler(this);
         displayQuests();
         setSelectedQuest();
+        //scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
     }
     public void displayQuests(){
         questListView.getItems().addAll(quests.keySet());
@@ -66,5 +71,54 @@ public class QuestListController implements Initializable {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    private void onSubscribeButtonClick() {
+        if (subscribeButton.isSelected()) {
+            mqttClient.connect(); // Connect to MQTT broker
+            mqttClient.subscribe();
+
+        } else {
+            mqttClient.disconnect();
+
+            mqttMessage.getStyleClass().clear();
+            mqttMessage.setText("");
+        }
+    }
+    @FXML
+    private void onManualPublishClick() {
+        String message = "Hello from JavaFX";
+        mqttClient.publishMessage("/quietquest/sensor/motion", message);
+    }
+
+    @Override
+    public void updateUI(String message) {
+        Platform.runLater(() -> {
+            if ("Hello from JavaFX".equals(message)) {
+                mqttMessage.setText("Works!");
+                mqttMessage.getStyleClass().clear();
+                mqttMessage.getStyleClass().add("label-all-green");
+
+            } else if ("Hi people are coming".equals(message)) {
+                mqttMessage.setText(message);
+                mqttMessage.getStyleClass().clear();
+                mqttMessage.getStyleClass().add("label-all-green");
+
+            } else if ("LIAN ADD HERE".equals(message)) {
+                mqttDistanceMessage.setText("");
+                mqttDistanceMessage.getStyleClass().clear();
+                mqttDistanceMessage.getStyleClass().add("label-all-green");
+
+            } else if("Sensor is watching".equals(message)) {
+                mqttMessage.setText(message);
+                mqttMessage.getStyleClass().clear();
+                mqttMessage.getStyleClass().add("label-all-red");
+
+            } else {
+                mqttMessage.getStyleClass().clear();
+                mqttMessage.setText(message);
+            }
+        });
     }
 }

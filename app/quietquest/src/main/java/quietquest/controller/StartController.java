10aca8c50534353e6adce0688f2c81b1ce19c980
Lian Shi <lians@student.chalmers.se>
2024-04-19 11:47:24 +0200

    #29 Update Start View

--- app/quietquest/src/main/java/quietquest/controller/StartController.java
+++ app/quietquest/src/main/java/quietquest/controller/StartController.java
@@ -24,11 +24,22 @@ public class StartController extends BaseController {
     @FXML
     private Button helloButton;
 
+
+
     @FXML
     protected void onNewQuestButtonClick(ActionEvent event) throws IOException {
         FXMLLoader loader = getFxmlLoader(FxmlFile.CREATE_QUEST);
         root = loader.load();
 
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onQuestListButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader  loader = getFxmlLoader(FxmlFile.QUEST_LIST);
+        root = loader.load();
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);

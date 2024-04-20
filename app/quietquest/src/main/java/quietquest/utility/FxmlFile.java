969fef04245a518382062dc1da854acdeb063206
Tanya Nordh <tanyan@net.chalmers.se>
2024-04-20 11:11:18 +0200

    #4 Add quest-view.fxml to list

--- app/quietquest/src/main/java/quietquest/utility/FxmlFile.java
+++ app/quietquest/src/main/java/quietquest/utility/FxmlFile.java
@@ -5,4 +5,5 @@ public class FxmlFile {
   public static final String QUEST_LIST = "quest-list-view.fxml";
   public static final String CREATE_QUEST = "create-quest-view.fxml";
   public static final String DELETE_QUEST = "delete-quest-view.fxml";
+  public static final String VIEW_QUEST = "quest-view.fxml";
 }

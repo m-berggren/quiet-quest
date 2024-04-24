afabb4f05c24df45a7160814f968fd2d59f07f0e
Marcus Berggren <marbergg@student.chalmers.se>
2024-04-25 01:23:38 +0200

    #32 Update utils cpp files

--- wio/quietquest/utils.cpp
+++ wio/quietquest/utils.cpp
@@ -1,39 +1,32 @@
 #include "utils.h"     // Include header file
 
 /**
+ * Aimed to reduce side effects delay() function has that it stops most activity on the terminal.
+ * Ideas adapted from below site. Millis() returns millisecons passed since wio terminal ran the program
+ * https://forum.arduino.cc/t/using-millis-for-timing-a-beginners-guide/483573
+*/
+bool isTimeToUpdate() {
+    static unsigned long startMillis = millis();          // Initialize once and remember across calls
+    unsigned long currentMillis = millis();             // Reduce calls to millis()
+    if (currentMillis - startMillis >= INTERVAL) {      // Checks whether period has passed
+        startMillis = currentMillis;                    // 
+        return true;
+    }
+    return false;
+}
+
+/**
  * Map from low-max value of the light sensor to a readable 0-100 percentage value,
  * as the raw signal data is not a generalized format.
  * @param lightValue - signal coming from light sensor
  * @return int - mapped value
 */
-int mapToPercentage(int lightValue) {
-    return map(lightValue, MIN_READING, MAX_READING, 0, 100)
+int mapToPercentage(int lightValue) { // map not functioning 
+    return map(lightValue, MIN_READING, MAX_READING, 0, 100);
 }
 
-/**
- * Parse digital value signal to more readable format.
- * @param digitalValue - digital signal, either 0 or 1
- * @return char* - parsed motion signal
-*/
-char* parseDigitalValue(int digitalValue) {
-    if (digitalValue == 0) {
-        return "false";
-    }
-    return "true";
-}
-
-/**
- * Aimed to reduce side effects delay() function has that it stops most activity on the terminal.
- * Ideas adapted from below site.
- * https://forum.arduino.cc/t/using-millis-for-timing-a-beginners-guide/483573
-*/
-bool isTimeToUpdate() {
-    static unsigned long startTime = millis();      // Initialize once and remember across calls
-    unsigned long currentMillis = millis();         // Reduce calls to millis()
-    if (currentMillis - startTime >= INTERVAL) {    // Checks whether period has passed
-        startTime = currentMillis;
-        return true;
-    }
-    return false;
-
+char* toString(int value) {
+    static char strValue[4];        // Static for retaining data between calls. Memory is allocated once and the pointer can point to valid memory slot
+    sprintf(strValue, "%d", value);
+    return strValue;
 }
--- wio/quietquest/utils.h
+++ wio/quietquest/utils.h
@@ -1,16 +1,21 @@
 #ifndef UTILS_H     //
 #define UTILS_H
 
-#define BAUD_RATE 9600      // Common data rate in bits per second
-#define MAX_READING 1023    // Max reading for light sensor
-#define MIN_READING 0       // Min reading value
-#define WIDTH 320           // Define the width of the Wio terminal display
-#define HEIGHT 240          // Define the height of the Wio terminal display
-#define INTERVAL 1000       // Stndard interval in ms
+#include <stdio.h>                          // Import needed for sprintf()
+#include <Arduino.h>                        // Import needed for map() and millis()
 
-// Sensor handling
-extern int mapToPercentage(int lightValue);
-extern char* parseDigitalValue(int digitalValue);
+#define BAUD_RATE   9600                    // Common data rate in bits per second
+#define PORT        1883                    // Standard port for connecting to broker
+#define SERVER      "broker.hivemq.com"     // Standard server for connectiong to broker
+#define MAX_READING 1023                    // Max reading for light sensor
+#define MIN_READING 0                       // Min reading value
+#define WIDTH       320                     // Define the width of the Wio terminal display
+#define HEIGHT      240                     // Define the height of the Wio terminal display
+#define INTERVAL    1000                    // Interval in ms to determine when to do prints and update LCD
+
 extern bool isTimeToUpdate();
+extern int mapToPercentage(int lightValue);
+extern bool isTimeToUpdate();
+extern char* toString(int integer);
 
 #endif

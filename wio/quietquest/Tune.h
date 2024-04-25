83b6fe1ff0c2cdd377a80eb39bc941e174b73e03
Tanya Nordh <tanyan@net.chalmers.se>
2024-04-25 11:03:28 +0200

    #7 Add audio alert for short, mid and long range

---
+++ wio/quietquest/Tune.h
@@ -0,0 +1,11 @@
+class Tune {
+    public:
+        void playTune(int, char[], int[], int); //length, notes, beats, tempo
+        void playNote(char, int); //note, duration
+        void playTone(int, int); //tone, duration
+};
+
+extern Tune longRange;
+extern Tune midRange;
+extern Tune shortRange;
+

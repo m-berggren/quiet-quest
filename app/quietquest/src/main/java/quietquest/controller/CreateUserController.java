b72b8940a4efc7bd0c4b7e7a909d4a2ab51f61d7
Tanya Nordh <tanyan@net.chalmers.se>
2024-05-01 15:17:22 +0200

    #48 New user created

--- app/quietquest/src/main/java/quietquest/controller/CreateUserController.java
+++ app/quietquest/src/main/java/quietquest/controller/CreateUserController.java
@@ -2,10 +2,22 @@ package quietquest.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.w3c.dom.Text;
+import quietquest.QuietQuestMain;
+import quietquest.model.Database;
+import quietquest.model.User;
+import quietquest.utility.FxmlFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class CreateUserController extends BaseController{
     @FXML
@@ -16,12 +28,28 @@ public class CreateUserController extends BaseController{
     private Button saveButton;
 
 
-    public void onSaveClick(ActionEvent event) {
-        //set created_at current datetime
-        //set app_sound ON
-        //set sensor_sound ON
-        //set desk_mode OFF
-        //insert user into database
-        //log in user and go to start page
+    public void onSaveClick(ActionEvent event) throws SQLException, IOException {
+        User user = new User(usernameTextField.getText(), passwordField.getText());
+        Database database = new Database();
+        if (database.createUser(user)) {
+            System.out.println("Successfully created user.");
+            loadFxml("create-quest-view.fxml", event);
+        } else {
+            System.out.println("Something went wrong.");
+        }
+        database.disconnect();
+    }
+
+    private void loadFxml(String fxmlFile, ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(QuietQuestMain.class.getResource(FxmlFile.MAIN));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm()); // adding CSS styling option
+        stage.setScene(scene);
+        stage.show();
+
+        MainController controller = loader.getController();
+        controller.loadView(fxmlFile);
     }
 }

32bd43170cbb27873970b5d96b960a9d6cb8f3f6
Tanya Nordh <tanyan@net.chalmers.se>
2024-05-02 10:05:26 +0200

    #48 Require unique username and valid password for user creation

--- app/quietquest/src/main/java/quietquest/controller/CreateUserController.java
+++ app/quietquest/src/main/java/quietquest/controller/CreateUserController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -26,18 +27,68 @@ public class CreateUserController extends BaseController{
     private PasswordField passwordField;
     @FXML
     private Button saveButton;
+    @FXML
+    private Label usernameExistsLabel;
+    @FXML
+    private Label passwordHintLabel;
 
 
+    /**
+     * Saves a new user to the database if a unique username and valid password are provided.
+     * @param event
+     * @throws SQLException
+     * @throws IOException
+     */
     public void onSaveClick(ActionEvent event) throws SQLException, IOException {
-        User user = new User(usernameTextField.getText(), passwordField.getText());
         Database database = new Database();
-        if (database.createUser(user)) {
-            System.out.println("Successfully created user.");
-            loadFxml("create-quest-view.fxml", event);
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
+        boolean usernameOK = false;
+        boolean passwordOK = false;
+
+        if (database.checkIfUsernameExists(username)) {
+            usernameExistsLabel.setVisible(true);
         } else {
-            System.out.println("Something went wrong.");
+            usernameOK = true;
+            usernameExistsLabel.setVisible(false);
         }
+
+        if (!passwordValid(password) || password.isEmpty()) {
+            passwordHintLabel.setVisible(true);
+        } else {
+            passwordOK = true;
+            passwordHintLabel.setVisible(false);
+        }
+
+        if(usernameOK && passwordOK){
+            User user = new User(username, password);
+
+            if (database.createUser(user)) {
+                System.out.println("Successfully created user.");
+                loadFxml("create-quest-view.fxml", event);
+            } else {
+                System.out.println("Something went wrong.");
+            }
+        }
+
         database.disconnect();
+    }
+
+    /**
+     * Checks validity of new password.
+     * @param password String that should contain at least 8 characters where at least one must be a number.
+     * @return True if password fulfills criteria, otherwise false.
+     */
+    public boolean passwordValid(String password){
+        boolean length = password.length() >= 8;
+        boolean containsNumber = false;
+        for(int i = 0; i < password.length(); i++){
+            if(Character.isDigit(password.charAt(i))){
+                containsNumber = true;
+                i = password.length();
+            }
+        }
+        return length && containsNumber;
     }
 
     private void loadFxml(String fxmlFile, ActionEvent event) throws IOException {

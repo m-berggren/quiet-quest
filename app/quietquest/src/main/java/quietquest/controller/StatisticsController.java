238b5b048c6ce8c572a1585094a7c5ab67fdbe53
Lian Shi <lians@student.chalmers.se>
2024-05-08 08:32:57 +0200

    #23 Add linked statistics button in quest history view

--- app/quietquest/src/main/java/quietquest/controller/StatisticsController.java
+++ app/quietquest/src/main/java/quietquest/controller/StatisticsController.java
@@ -1,5 +1,20 @@
 package quietquest.controller;
 
-public class StatisticsController {
+import quietquest.model.Quest;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StatisticsController extends BaseController{
+  private ArrayList<Quest> quests;
+
+  public void afterMainController() throws SQLException {
+    database.connect();
+    quests = database.getAllQuests(user);
+    database.disconnect();
+
+
+  }
 
 }

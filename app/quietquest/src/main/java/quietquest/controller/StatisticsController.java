85d39c68511b1a1ca6f248b8c48fd1bc37d9f878
Lian Shi <lians@student.chalmers.se>
2024-05-09 14:45:23 +0200

    #52 Add statistics methods to calculate data

--- app/quietquest/src/main/java/quietquest/controller/StatisticsController.java
+++ app/quietquest/src/main/java/quietquest/controller/StatisticsController.java
@@ -4,7 +4,6 @@ import quietquest.model.Quest;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class StatisticsController extends BaseController{
   private ArrayList<Quest> quests;
@@ -13,8 +12,68 @@ public class StatisticsController extends BaseController{
     database.connect();
     quests = database.getAllQuests(user);
     database.disconnect();
+    displayStatistics();
+  }
+
+  public int getCompletedQuestsNumber() {
+    int completedQuests = 0;
+    for (Quest quest : quests) {
+      if (quest.getCompletionState()) {
+        completedQuests++;
+      }
+    }
+    return completedQuests;
+  }
+
+  public int getInProgressQuestsNumber() {
+    int inProgressQuests = 0;
+    for (Quest quest : quests) {
+      if (!quest.getCompletionState()) {
+        inProgressQuests++;
+      }
+    }
+    return inProgressQuests;
+  }
+
+  public int getCreatedQuestsNumber() {
+    return quests.size();
+  }
+
+  public int getAverageOpenBoxTimes() {
+    int totalOpenBoxTimes = 0;
+    for (Quest quest : quests) {
+      totalOpenBoxTimes += quest.getBoxOpenTimes();
+    }
+    return totalOpenBoxTimes / quests.size();
+  }
+
+  public int getAverageOpenBoxInterval(){
+    int totalQuestTimeSpent = 0;
+    int totalBoxOpenTimes = 0;
+    int averageOpenBoxInterval = 0;
+    for (Quest quest : quests) {
+      int timeSpent = (int) ((quest.getEndTime().getTime() - quest.getStartTime().getTime()) / 60000);
+      totalQuestTimeSpent += timeSpent;
+      totalBoxOpenTimes += quest.getBoxOpenTimes() + 1;
+    }
+    averageOpenBoxInterval = totalQuestTimeSpent / totalBoxOpenTimes;
+    return totalQuestTimeSpent;
+  }
+
+  //Calculate the average time spent on a quest
+  public int getAverageTimeSpentOnQuest() {
+    int totalQuestTimeSpent = 0;
+    for (Quest quest : quests) {
+      int timeSpent = (int) ((quest.getEndTime().getTime() - quest.getStartTime().getTime()) / 60000);
+      totalQuestTimeSpent += timeSpent;
+    }
+    return totalQuestTimeSpent / quests.size();
+  }
+
+  public void displayStatistics() {
+    // Display above statistics in the view
+
 
 
   }
-
 }
